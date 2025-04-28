3db25ea9136b157494e74db74af8f1e7b36a75de
Sakeelb <sakeelb115@gmail.com>
2025-04-28 20:40:51 +0530

    Update keep_alive.py

--- New Folder/keep_alive.py
+++ New Folder/keep_alive.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Bot is Running - Boss Mode"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 8080))  # Render.com का PORT इस्तेमाल करें
+    )
 
 def keep_alive():
     t = Thread(target=run)
